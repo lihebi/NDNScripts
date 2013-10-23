f9651a015078ca92f12b82dcf138eaf232711554
hebi <lihebi@mail.ustc.edu.cn>
2013-10-23 18:12:45 +0800

    multiple interface

--- interface_conf.sh
+++ interface_conf.sh
@@ -6,29 +6,48 @@
 
 sudo modprobe ip_gre
 cd /etc/network
-sudo mv interfaces interfaces-bak
-read -p 'enter interface name:>> ' interface
-read -p 'enter my private ip:>> ' myprivateip
-read -p 'enter your private ip:>> ' yourprivateip
-read -p 'enter my ip:>> ' myip
-read -p 'enter your ip:>> ' yourip
-interface=$HOME-interface
-myprivateip=10.0.1.9
-yourprivateip=10.0.1.10
-myip=192.168.1.108
-yourip=192.168.1.109
+myip=192.168.16.7
+face0_name=face0
+face0_mygre=10.0.1.9
+face0_remotegre=10.0.1.12
+face0_remoteip=192.168.16.11
+face1_name=face1
+face1_mygre=10.0.1.10
+face1_remotegre=10.0.1.17
+face1_remoteip=192.168.16.10
+face2_name=face2
+face2_mygre=10.0.1.11
+face2_remotegre=10.0.1.20
+face2_remoteip=192.168.1.128
 cd
 cat > interfaces <<EOF
 auto lo
 iface lo inet loopback
 
-auto $interface
-iface $interface inet static
-address $myprivateip
-netmask 255.255.255.252
-pre-up iptunnel add $interface mode gre local $myip remote $yourip ttl 255
-pointopoint $yourprivateip
-up ifconfig $interface multicast
+auto $face0_name
+iface $face0_name inet static
+address $face0_mygre
+netmask 255.255.255.0
+pre-up iptunnel add $face0_name mode gre local $myip remote $face0_remoteip ttl 255
+pointopoint $face0_remotegre
+up ifconfig $face0_name multicast
+
+auto $face1_name
+iface $face1_name inet static
+address $face1_mygre
+netmask 255.255.255.0
+pre-up iptunnel add $face1_name mode gre local $myip remote $face1_remoteip ttl 255
+pointopoint $face1_remotegre
+up ifconfig $face1_name multicast
+
+#auto $face2_name
+#iface $face2_name inet static
+#address $face2_mygre
+#netmask 255.255.255.0
+#pre-up iptunnel add $face2_name mode gre local $myip remote $face2_remoteip ttl 255
+#pointopoint $face2_remotegre
+#up ifconfig $face2_name multicast
+
 EOF
 sudo mv interfaces /etc/network/
 sudo /etc/init.d/networking restart
