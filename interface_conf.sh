7c17300531dd4aa543af8e279b807a2730c81757
Hebi <lihebi@mail.ustc.edu.cn>
2013-10-26 21:04:40 +0800

    tmp

--- interface_conf.sh
+++ interface_conf.sh
@@ -6,14 +6,14 @@
 
 sudo modprobe ip_gre
 cd /etc/network
-myip=192.168.16.7
+myip=192.168.16.8
 face0_name=face0
 face0_mygre=10.0.1.9
-face0_remotegre=10.0.1.12
-face0_remoteip=192.168.16.11
+face0_remotegre=10.0.1.10
+face0_remoteip=192.168.16.7
 face1_name=face1
-face1_mygre=10.0.1.10
-face1_remotegre=10.0.1.17
+face1_mygre=10.0.1.12
+face1_remotegre=10.0.1.11
 face1_remoteip=192.168.16.10
 face2_name=face2
 face2_mygre=10.0.1.11
@@ -32,13 +32,13 @@ pre-up iptunnel add $face0_name mode gre local $myip remote $face0_remoteip ttl 
 pointopoint $face0_remotegre
 up ifconfig $face0_name multicast
 
-auto $face1_name
-iface $face1_name inet static
-address $face1_mygre
-netmask 255.255.255.0
-pre-up iptunnel add $face1_name mode gre local $myip remote $face1_remoteip ttl 255
-pointopoint $face1_remotegre
-up ifconfig $face1_name multicast
+#auto $face1_name
+#iface $face1_name inet static
+#address $face1_mygre
+#netmask 255.255.255.0
+#pre-up iptunnel add $face1_name mode gre local $myip remote $face1_remoteip ttl 255
+#pointopoint $face1_remotegre
+#up ifconfig $face1_name multicast
 
 #auto $face2_name
 #iface $face2_name inet static
--- ospf_conf.sh
+++ ospf_conf.sh
@@ -4,16 +4,18 @@
 # date: 10/16/2013
 # all rights reserved
 # ================
-myhost=$HOME.host
+myhost=PC08.host
 logfile=/tmp/ospfd.log
-interface=$HOME-interface #maybe need increase
-routerid=192.168.0.1 #need modify
+#interface=$HOME-interface #maybe need increase
+routerid=192.168.16.8 #need modify
 cat > ospfd.conf <<EOF
 hostname $myhost
 password pwd
 enable password pwd
 log file $logfile
-interface $interface
+!interface $interface
+interface face0
+interface face1
 router ospf
  ospf router-id $routerid
  redistribute connected
