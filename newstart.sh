1bb9b4e1d3e5921e870517f83b9c5e0e2f441c03
Hebi <lihebi@mail.ustc.edu.cn>
2013-10-27 15:16:39 +0800

    new attempt

--- newstart.sh
+++ newstart.sh
@@ -1,9 +1,12 @@
 #!/bin/sh
+echo 'starting zebra...'
 sudo zebra -d
 sleep 5
+echo 'starting ospfd...'
 sudo ospfd -d -a
 sleep 5
 ccndstart
+echo 'starting ospfn...'
 sleep 5
 cd $ospfn
 sudo ospfn -d
